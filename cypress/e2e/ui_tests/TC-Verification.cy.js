2bfb02d978c48aaf892a3438dd7b842a7829ab9f
idham099 <ainul.idham_sttpln@yahoo.com>
2025-11-11 20:45:58 +0800

    Initial commit: Add baru

--- cypress/e2e/ui_tests/TC-Verification.cy.js
+++ cypress/e2e/ui_tests/TC-Verification.cy.js
@@ -1,5 +1,8 @@
 import HomePage from '../../support/pageObjects/HomePage'
 import Navigation from '../../support/pageObjects/Navigation'
+import users from '../../support/data/users.json'
+
+const CheckOut = users.checkout;
 
 describe('Modul: User Management', () => {
     it('[Positive] TC_P004: Lihat Detail Produk dari Homepage.', () => {
@@ -185,6 +188,54 @@ describe('Modul: User Management', () => {
         cy.get(Navigation.KlikPlaceOrder).should('be.visible').click();
 
         //Then Muncul modal pop-up "Place order" (form pengisian data).
+
+        // Wait untuk memastikan request asli telah selesai diproses oleh backend
+        cy.wait('@NavRequest')         
+    }),
+
+    it('[Positive] TC_CH002: Isi Data Pembelian dan Lanjut (Purchase).', () => {
+        
+        cy.intercept('GET', '**/entries').as('NavRequest');
+        const expectedAlertText = "Product added";
+
+        // Given pengguna berada di Halaman Utama
+        HomePage.visit()
+      
+        // When pengguna mengklik Klik Navigation phone 
+        cy.get(Navigation.Klik).should('be.visible').click(); 
+        
+        cy.wait(500);
+
+        //And List phone muncul
+        
+
+        //And Pengguna melakukan klik pada produk
+        cy.get(Navigation.KlikProduk).should('be.visible').click(); 
+
+        //And Halaman Detail Produk dimuat, menampilkan gambar, deskripsi, dan harga yang benar.
+
+        //And Pengguna Klik tombol "Add to cart"
+        cy.get(Navigation.KlikCart).should('be.visible').click(); 
+
+        //And Muncul alert (popup) browser "Product added".
+         cy.window().then((win) => {
+            cy.stub(win, 'alert').as('alertStub');
+        });         
+        cy.get('@alertStub')
+            .should('be.calledOnce') 
+            .and('be.calledWith', expectedAlertText);
+        
+        //And Item telah ditambahkan ke Cart (TC_P005 berhasil).
+        cy.get(Navigation.KlikLinkCart).should('be.visible').click();
+        
+        //And Pengguna klik tombol "Place Order".
+        cy.get(Navigation.KlikPlaceOrder).should('be.visible').click();
+
+        //And Muncul modal pop-up "Place order" (form pengisian data).
+        Navigation.fillOrder(Navigation.name, Navigation.country, Navigation.city, Navigation.card, Navigation.month, Navigation.year)
+        
+        //And pengguna Klik tombol "Purchase"
+        Navigation.submit()
 
         // Wait untuk memastikan request asli telah selesai diproses oleh backend
         cy.wait('@NavRequest')         
--- cypress/support/pageObjects/Navigation.js
+++ cypress/support/pageObjects/Navigation.js
@@ -8,5 +8,28 @@ class Navigation {
     get KlikLinkCart() { return '#cartur' }
     get DeleteCart() { return 'body > div:nth-child(7) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(4) > a:nth-child(1)' }
     get KlikPlaceOrder() { return '.btn.btn-success' }
+
+    get Name() { return '#name' }
+    get Country() { return '#country' }
+    get City() { return '#city' }
+    get Card() { return '#card' }
+    get Month() { return '#month' }
+    get Year() { return '#year' }
+    get Purchase() { return 'button[onclick="purchaseOrder()"]' }
+
+
+    fillOrder(name, country, city, card, month, year) {
+        cy.get(this.Name).type(name)
+        cy.get(this.Country).type(country)
+        cy.get(this.City).type(city)
+        cy.get(this.Card).type(card)
+        cy.get(this.Month).type(month)
+        cy.get(this.Year).type(year)
+    }
+
+    submit() {
+        cy.get(this.Purchase).click()
+    }
+
 }
 export default new Navigation()
